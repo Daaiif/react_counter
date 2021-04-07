f7f309c94e3f49007bd6e5ba41c8d02126f84c9e
Makhmut <grupry@gmail.com>
2021-04-07 14:48:49 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.d6e14de36f6c154537366d288deafbe0.js"
 );
 
 self.addEventListener('message', (event) => {
